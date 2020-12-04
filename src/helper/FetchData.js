b23eae07459754161792b68ee302a6ed2d82e330
faris <faris38kanbur@gmail.com>
2020-12-04 21:53:56 +0100

    add API call user

---
+++ src/helper/FetchData.js
@@ -0,0 +1,20 @@
+// import axios from 'axios';
+
+
+// const {REACT_APP_API_BASE_URL, REACT_APP_API_TOKEN} = process.env;
+
+//   export const getAPI = async() =>{
+//         const response = await axios.get(`${REACT_APP_API_BASE_URL}/user`, {
+//              headers: {
+//                  'app-id': REACT_APP_API_TOKEN
+//              },
+//          });
+//          return response?.data?.data;
+//      };
+
+
+// export const fetchData = () =>{
+//     const result = getAPI()
+
+//     return result;
+// }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import {Container} from "@material-ui/core";
+import axios from 'axios';
 
 
 const stylesFunc = makeStyles((theme) => ({
@@ -16,10 +17,41 @@ const stylesFunc = makeStyles((theme) => ({
   }));
 
 function Main() {
+    const [userList, setUserList] = useState();
     const mainStyles = stylesFunc();
+
+    const {REACT_APP_API_BASE_URL, REACT_APP_API_TOKEN} = process.env;
+
+    const fetchData = async() =>{
+       const response = await axios.get(`${REACT_APP_API_BASE_URL}/user`, {
+            headers: {
+                'app-id': REACT_APP_API_TOKEN
+            },
+        });
+        setUserList(response?.data?.data);
+    };
+
+    useEffect(() =>{
+        fetchData();
+    }, [])
+
+
+    // useEffect(() =>{
+    //     const result = fetchData();
+    //     setUserList(result);
+    // }, []);
+
+
+
+
+
     return (
         <Container className={mainStyles.wrapper} maxWidth="sm">
-            MAIN PAGE
+            {userList?.map((user) => {
+                return <p 
+                        key={user?.id}
+                        >{`${user.title} ${user.firstName} ${user.lastName}`}</p>
+            })}
         </Container>
     )
 }
